c1654a00857424d771b9f211696388a0d4610c7b
Jean-Philippe Gravel <jpgravel@elogicgroup.com>
2018-07-25 13:02:31 -0400

    Adding VisibleWidth

--- Formix.Games.Tetris/display/Sprite.cs
+++ Formix.Games.Tetris/display/Sprite.cs
@@ -12,6 +12,22 @@ namespace Formix.Games.Tetris.Display
         public int Col { get; set; }
         public int Height { get => Content.GetUpperBound(0) + 1; }
         public int Width { get => Content.GetUpperBound(1) + 1; }
+
+        public int VisibleWidth
+        {
+            get
+            {
+
+            }
+        }
+
+        public int VisibleHeight
+        {
+            get
+            {
+            }
+        }
+
         public ColoredChar[,] Content { get; private set; }
 
         public Sprite(int height, int width, string name = null)
